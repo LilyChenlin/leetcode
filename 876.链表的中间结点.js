a7834ceb7cb244b119a09af5c0b747ff2e287a02
LilyChenlin <906791425@qq.com>
2020-11-02 20:36:29 +0800

    链表-链表的中间节点

---
+++ 876.链表的中间结点.js
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=javascript
+ *
+ * [876] 链表的中间结点
+ *
+ * https://leetcode-cn.com/problems/middle-of-the-linked-list/description/
+ *
+ * algorithms
+ * Easy (69.56%)
+ * Likes:    273
+ * Dislikes: 0
+ * Total Accepted:    81.3K
+ * Total Submissions: 116.8K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ * 
+ * 如果有两个中间结点，则返回第二个中间结点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next
+ * = NULL.
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：[1,2,3,4,5,6]
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+ * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 给定链表的结点数介于 1 和 100 之间。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let length = 1;
+    let newhead = head;
+    while(newhead.next){
+        length++;
+        newhead=newhead.next;
+    }
+    length%2 == 0 ? length = length/2+1 : length = (length+1)/2;
+    while(--length){
+        head=head.next
+    }
+    return head
+};
+
+// 快慢指针
+var middleNode = function(head) {
+    var slow = head;
+    var fast = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+}
+// @lc code=end
+
+ 
