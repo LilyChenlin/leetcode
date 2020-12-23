6b72f5822aba19dc0c79cf1cff93d092658655a9
LilyChenlin <906791425@qq.com>
2020-12-23 14:03:15 +0800

    add: Set实现两个数组的交集

---
+++ 349.两个数组的交集.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=javascript
+ *
+ * [349] 两个数组的交集
+ *
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (71.06%)
+ * Likes:    301
+ * Dislikes: 0
+ * Total Accepted:    146.2K
+ * Total Submissions: 199.4K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ * 
+ * 
+ * 
+ * 说明：
+ * 
+ * 
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    if(!nums1.length || !nums2.length) {
+        return [];
+    }
+    let set1 = new Set(nums1);
+    let set2 = new Set();
+    nums2.forEach(item => {
+        if (set1.has(item)) {
+            set2.add(item)
+        }
+    })
+    return [...set2]; // 将Set数据结构转化为数组
+};
+// @lc code=end
+
