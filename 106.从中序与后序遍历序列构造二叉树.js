98e77b24f806c31f59758af2e960d96d5a5c6e5a
LilyChenlin <906791425@qq.com>
2020-12-26 21:13:16 +0800

    add: 二叉树-从中序与后序遍历序列构造二叉树

---
+++ 106.从中序与后序遍历序列构造二叉树.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=106 lang=javascript
+ *
+ * [106] 从中序与后序遍历序列构造二叉树
+ *
+ * https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+ *
+ * algorithms
+ * Medium (70.74%)
+ * Likes:    422
+ * Dislikes: 0
+ * Total Accepted:    81.9K
+ * Total Submissions: 115.5K
+ * Testcase Example:  '[9,3,15,20,7]\n[9,15,7,20,3]'
+ *
+ * 根据一棵树的中序遍历与后序遍历构造二叉树。
+ * 
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ * 
+ * 例如，给出
+ * 
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 后序遍历 postorder = [9,15,7,20,3]
+ * 
+ * 返回如下的二叉树：
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+var buildTree = function(inorder, postorder) {
+    return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1)
+};
+var build = function (inorder, inStart, inEnd, postorder, postStart, postEnd) {
+    if (inStart > inEnd) return null;
+    let rootVal = postorder[postEnd];
+    let index = inorder.indexOf(rootVal);
+
+    let root = new TreeNode(rootVal);
+    let leftSize = index - inStart;
+    root.left = build(inorder, inStart, index - 1, postorder, postStart, leftSize + postStart - 1);
+    root.right = build(inorder, index + 1, inEnd, postorder, postStart + leftSize, postEnd - 1)
+    return root;
+}
+// @lc code=end
+
