4c33d79052b805c5707c5bc45efc43171c755acc
LilyChenlin <906791425@qq.com>
2021-03-21 20:33:43 +0800

    add: 495提莫攻击使用倒序

--- 495.提莫攻击.js
+++ 495.提莫攻击.js
@@ -10,6 +10,23 @@
  * @param {number} duration
  * @return {number}
  */
+
+// 倒序遍历
+// [1, 2, 3, 4] duration: 2
+// [1, 4] duration: 2
+var findPoisonedDuration = function(timeSeries, duration) {
+    if (timeSeries.length == 0) return 0;
+    var res = duration;
+    for (var i = timeSeries.length - 2; i>=0; i--) {
+        if (timeSeries[i] + duration > timeSeries[i + 1]) {
+            res += (timeSeries[i + 1] - timeSeries[i])
+        } else {
+            res += duration;
+        }
+    }
+    return res;
+};
+
 var findPoisonedDuration = function(timeSeries, duration) {
     var res = 0;
     for (let i = 0; i < timeSeries.length; i++) {
