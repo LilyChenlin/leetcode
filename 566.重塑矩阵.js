0d1557f8f6691b0849945dd303fbc8fe1414bb0c
LilyChenlin <906791425@qq.com>
2021-04-07 21:13:08 +0800

    feat: 解法二 重塑矩阵

--- 566.重塑矩阵.js
+++ 566.重塑矩阵.js
@@ -12,25 +12,37 @@
  * @return {number[][]}
  */
 // 解法一
+// var matrixReshape = function(nums, r, c) {
+//     let row = nums.length, col = nums[0].length;
+//     const rowCol = row * col;
+//     const rc = r * c;
+//     if (rowCol < rc) { return nums}
+//     let result = new Array(r).fill(0).map(() => new Array(c).fill(0));
+//     let mrow = 0, ncol = 0;
+//     for (let i = 0; i < r; i++) {
+//         for (let j = 0; j < c; j++) {
+//             result[i][j] = nums[mrow][ncol++];
+//             if (ncol == col) {
+//                 ncol = 0;
+//                 mrow++;
+//             }
+//         }
+//     }
+
+//     return result;
+// };
+// 解法二：二维数组的一维表示
 var matrixReshape = function(nums, r, c) {
     let row = nums.length, col = nums[0].length;
     const rowCol = row * col;
     const rc = r * c;
     if (rowCol < rc) { return nums}
     let result = new Array(r).fill(0).map(() => new Array(c).fill(0));
-    let mrow = 0, ncol = 0;
-    for (let i = 0; i < r; i++) {
-        for (let j = 0; j < c; j++) {
-            result[i][j] = nums[mrow][ncol++];
-            if (ncol == col) {
-                ncol = 0;
-                mrow++;
-            }
-        }
+    for (let i = 0; i < rc; i++) {
+        result[Math.floor(i / c)][i % c] = nums[Math.floor(i / col)][i % col]
     }
 
     return result;
 };
-// 解法二：二维数组的一维表示
 // @lc code=end
 
