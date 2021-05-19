fea903b393b061f5ab41fb19b97376ef0b5cfcfd
LilyChenlin <906791425@qq.com>
2021-05-19 10:56:36 +0800

    feat: 字符串相乘方法更改

--- 43.字符串相乘.js
+++ 43.字符串相乘.js
@@ -10,30 +10,33 @@
  * @param {string} num2
  * @return {string}
  */
-var multiply = function(num1, num2) {
-    let res = 0;
-    let j = 0; // 判断乘法位数
-    for (let i = num2.length - 1; i >= 0; i--) {
-        const number = Number(num2[i]);
-        let temp = multiplyOne(num1, number);
-        temp = temp + (j > 0 ? '0'.repeat(j) : '');
-        res = res + Number(temp);
-        j++;
+var multiply = function (num1, num2) {
+    if (num1 == '0' || num2 == '0') {
+        return '0';
     }
-    return res.toString();
-};
-        // "012", "189"
-var multiplyOne = function (num1, number) {
-    // 判断是否进位
-    let flag = 0; 
+    let len1 = num1.length, len2 = num2.length;
+    let ansArr = new Array(len1 + len2).fill(0);
+    for (let i = len1 - 1; i >= 0; i--) {
+        let x = Number(num1[i]);
+        for (let j = len2 - 1; j >= 0; j--) {
+            let y = Number(num2[j]);
+            ansArr[i + j + 1] += x * y;
+        }
+    }
+    // console.log(ansArr)
+    // 处理 进位
+    for (let i = len1 + len2 - 1; i > 0; i--) {
+        ansArr[i - 1] += Math.floor(ansArr[i] / 10);
+        ansArr[i] = ansArr[i] % 10;
+    }
+    
+    let i = ansArr[0] == '0' ? 1 : 0;
     let res = '';
-    for (let i = num1.length - 1; i >= 0; i--) {
-        let sum = Number(num1[i]) * number + flag;
-        res = res + sum % 10;
-        flag = Math.floor(sum / 10);
+    while (i < ansArr.length) {
+        res += ansArr[i];
+        i++
     }
-    res += flag !== 0 ? flag : '';
-    return res.split("").reverse().join("");
+    return res;
 }
 // @lc code=end
 
