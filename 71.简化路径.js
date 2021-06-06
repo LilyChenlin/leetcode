7c56690d177c1c58106175c7dae5754523862dce
LilyChenlin <906791425@qq.com>
2021-06-06 21:22:37 +0800

    两种解法

--- 71.简化路径.js
+++ 71.简化路径.js
@@ -9,6 +9,59 @@
  * @param {string} path
  * @return {string}
  */
+var simplifyPath = function(path) {
+    // 取出目录名->英文字母
+    let engList = [];
+    let temp = "";
+    let numDian = 0;
+    for (let i = 0; i < path.length; i++) {
+        if (path[i] !== '/' && path[i] !== '.') {
+            if (numDian > 0) {
+                temp = temp + '.'.repeat(numDian)
+                numDian = 0
+            }
+            // 英文字母
+            temp = temp + path[i];
+        } else if (path[i] == '.') {
+            numDian++;
+
+        } else {// path[i] == '/'
+            let copy = '';
+            if (temp !== '' && numDian !== 0) {
+                copy = temp + '.'.repeat(numDian);
+                engList.push(copy);
+                numDian = 0;
+            } 
+            if (numDian ==  2) {
+                engList.pop();
+            } else if (numDian > 2) {
+                engList.push('.'.repeat(numDian));
+            }
+            if (temp !== '' && numDian == 0 && copy == '') {
+                engList.push(temp);
+            }
+            numDian = 0;
+            temp = '';
+            copy = ''
+        }
+    }
+    if (temp) engList.push(temp);
+    let res = '/';
+    if (engList.length == 0 && numDian == 0) {
+        return res;
+    }
+    if (numDian == 2) {
+        engList.pop();
+    } else if (numDian > 2) {
+        engList.push('.'.repeat(numDian))
+    }
+    for (let i = 0; i < engList.length; i++) {
+        res = res + engList[i] + (i == engList.length - 1 ? '' : '/');
+    }
+
+    return res;
+};
+
 var simplifyPath = function(path) { 
     const dir = path.split('/'), stack = []
     for (const i of dir) {
@@ -21,6 +74,5 @@ var simplifyPath = function(path) {
     }
     return '/' + stack.join('/')
 }
-
 // @lc code=end
 
