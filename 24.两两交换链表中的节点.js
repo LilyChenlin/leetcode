f111bafbec3f04bf03592ab04525663f646307ab
LilyChenlin <906791425@qq.com>
2021-06-24 22:36:53 +0800

    feat: 递归/迭代实现两两交换链表中的节点

--- 24.两两交换链表中的节点.js
+++ 24.两两交换链表中的节点.js
@@ -16,19 +16,32 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+ // 递归方式
+// var swapPairs = function(head) {
+//     if (head == null || head.next == null) {return head};
+//     let newHead = head.next;
+//     head.next = swapPairs(newHead.next);
+//     newHead.next = head;
+//     return newHead;
+// };
+
+// 迭代方式
 var swapPairs = function(head) {
     const dummyNode = new ListNode(0);
     dummyNode.next = head;
     let temp = dummyNode;
-    if (temp.next !== null && temp.next.next !== null) {
-        const temp1 = temp.next;
-        const temp2 = temp.next.next;
-        temp.next = temp2;
-        temp1.next = temp2.next;
-        temp2.next = temp1;
-        temp = temp1;
+    while (temp.next !== null && temp.next.next !== null) {
+        const code1 = temp.next;
+        const code2 = temp.next.next;
+        temp.next = code2;
+        code1.next = code2.next;
+        code2.next = code1;
+        temp = code1;
     }
     return dummyNode.next;
 };
+
+
 // @lc code=end
 
