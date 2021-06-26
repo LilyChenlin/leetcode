c6f71a39ba5cfac39c84a73f9db408a68a94ff6c
LilyChenlin <906791425@qq.com>
2021-06-26 22:51:32 +0800

    fix: k个一组翻转链表

--- 25.k-个一组翻转链表.js
+++ 25.k-个一组翻转链表.js
@@ -19,37 +19,26 @@
  */
 var reverseKGroup = function(head, k) {
     if (head == null) return null;
-    var a = head, b = head;
+    let b = head, a = head;
     for (let i = 0; i < k; i++) {
-        // 如果不满足k个节点，则这个节点不需要反转，直接返回
         if (b == null) return head;
-        b = b.next; // 递归找到b节点
+        b = b.next;
     }
-    // 反转某个区间的链表
+
+    // 反转[a,b)区间的链表
     const newHead = reverse(a, b);
     a.next = reverseKGroup(b, k);
-    return newHead
+    return newHead;
 };
-
-// 反转[a,b) 
-/**
- * 
- * @param {ListNode} a 
- * @param {ListNode} b 
- */
-var reverse = function(a, b) {
-    var pre = null, next = a, cur = a;
-
-    // 终止条件
-    while(cur !== b) {
+const reverse = function(a, b) {
+    let pre = null, cur = a, next = a;
+    while (cur !== b) {
         next = cur.next;
         cur.next = pre;
         pre = cur;
         cur = next;
-    }
-
-    // 返回反转后的头节点
-    return pre
+    } 
+    return pre;
 }
 // @lc code=end
 
