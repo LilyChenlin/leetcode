8a2db1542ee64c6fb785e4c7bef0ff14abd17bd4
LilyChenlin <906791425@qq.com>
2021-07-20 22:11:47 +0800

    feat: 存在重复元素

---
+++ 219.存在重复元素-ii.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=javascript
+ *
+ * [219] 存在重复元素 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            if ((i - map.get(nums[i])) <= k){
+                return true
+            }
+        }
+        map.set(nums[i], i)
+    }
+    return false
+
+};
+// @lc code=end
+
