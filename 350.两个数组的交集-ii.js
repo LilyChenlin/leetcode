69a6a553b1058d61392a533519ebb9e1c4e1b8c1
LilyChenlin <906791425@qq.com>
2021-07-25 21:49:34 +0800

    feat: 两个数组的交集

--- 350.两个数组的交集-ii.js
+++ 350.两个数组的交集-ii.js
@@ -54,25 +54,17 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    if (!nums1.length || !nums2.length) {
-        return [];
+    let map = new Map();
+    for (let i = 0; i < nums1.length; i++) {
+        map.set(nums1[i], map.has(nums1[i]) ? map.get(nums1[i]) + 1 : 1);
     }
-    let map =  new Map();
     let res = [];
-    const len = nums1.length;
-    for (let i = 0; i <len; i++) {
-        if (map.has(nums1[i])) {
-            map.set(nums1[i], map.get(nums1[i]) + 1)
-        } else {
-            map.set(nums1[i], 1);
+    for (let i = 0; i < nums2.length; i++) {
+        if (map.has(nums2[i]) && map.get(nums2[i]) > 0) {
+            map.set(nums2[i], map.get(nums2[i]) - 1);
+            res.push(nums2[i])
         }
     }
-    nums2.forEach((item, index) => {
-        if (map.has(item) && map.get(item) > 0) {
-            res.push(item);
-            map.set(item, map.get(item) - 1)
-        }
-    })
     return res;
 };
 // @lc code=end
