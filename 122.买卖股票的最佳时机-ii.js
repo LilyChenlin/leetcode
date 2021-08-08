a124cb68f997a25d35b97450f6aad09083d48129
LilyChenlin <906791425@qq.com>
2021-08-08 21:17:47 +0800

    动态规划实现股票II

--- 122.买卖股票的最佳时机-ii.js
+++ 122.买卖股票的最佳时机-ii.js
@@ -10,12 +10,24 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let ans = 0;
-    let n = prices.length;
+    const n = prices.length;
+    // const dp = new Array(n).fill(0).map(item => new Array(n).fill(0));
+    // dp[0][0] = 0, dp[0][1] = -prices[0];
+    // for (let i = 1; i < n; i++) {
+    //     dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+    //     dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+    // }
+    // return dp[n - 1][0];
+
+    // 优化
+    let dp0 = 0, dp1 = -prices[0];
     for (let i = 1; i < n; i++) {
-        ans += Math.max(0, prices[i] - prices[i - 1])
+        let newDp0 = Math.max(dp0, dp1 + prices[i]);
+        let newDp1 = Math.max(dp1, dp0 - prices[i]);
+        dp0 = newDp0;
+        dp1 = newDp1
     }
-    return ans
+    return dp0;
 };
 // @lc code=end
 
