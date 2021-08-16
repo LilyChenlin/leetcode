cf77c6f33506f590a49c03ccd51ad313e9c93b81
LilyChenlin <906791425@qq.com>
2021-08-16 22:51:13 +0800

    feat: 56合并区间 妙

--- 56.合并区间.js
+++ 56.合并区间.js
@@ -11,18 +11,17 @@
  */
 var merge = function(intervals) {
     let res = [];
-    // 升序
-    intervals = intervals.sort((a, b) => {return a[0] - b[0]});
+    intervals = intervals.sort((a, b) =>  a[0] - b[0]);
     let i = 0;
-    while (i < intervals.length ) {
+    while (i < intervals.length) {
         let left = intervals[i][0];
         let right = intervals[i][1];
-        while (i < intervals.length - 1 && right >= intervals[i+1][0]) {
-            right = Math.max(right, intervals[i+1][1]);
+        while (i < intervals.length - 1&& right >= intervals[i + 1][0]) {
+            right = Math.max(right, intervals[i + 1][1]);
             i++;
         }
-        res.push([left, right]);
-        i++
+        res.push([left, right])
+        i++;
     }
     return res;
 };
