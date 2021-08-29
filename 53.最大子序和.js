165024d375a91d293e30bdf3ec7be2c19389223b
LilyChenlin <906791425@qq.com>
2021-08-29 21:21:09 +0800

    feat: 53最大子序和

---
+++ 53.最大子序和.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=javascript
+ *
+ * [53] 最大子序和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let ans = nums[0];
+    let total = 0; // 前缀和
+    for (let i = 1; i < nums.length; i++) {
+        if (total > 0) {
+            total += nums[i]
+        } else {
+            total = nums[i];
+        }
+        ans = Math.max(ans, total);
+    }
+    return ans;
+};
+// @lc code=end
+
