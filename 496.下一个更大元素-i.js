121d4c16b1001e199d182ce754c33a990545149d
LilyChenlin <906791425@qq.com>
2021-09-09 23:03:19 +0800

    feat: 496下一个更大的元素

--- 496.下一个更大元素-i.js
+++ 496.下一个更大元素-i.js
@@ -11,25 +11,20 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    for (let i = 0; i < nums1.length; i++) {
-        let num = nums1[i];
-        let idx = nums2.indexOf(num);
-        let sortArr = nums2.slice(idx) || [];
-        sortArr.sort((a, b) => a - b);
-        if ( sortArr.length > 0 && sortArr[sortArr.length - 1] > num) {
-            let j = idx;
-            while (j < nums2.length) {
-                if (nums2[j] > nums1[i]) {
-                    nums1[i] = nums2[j];
-                    break;
-                } 
-                j++
-            }
-        } else {
-            nums1[i] = -1;
+    // 找出nums2中的每一个元素的下一个更大的元素
+    let len1 = nums1.length, len2 = nums2.length;
+    let stack = [], map = new Map();
+    for (let i = 0; i < len2; i++) {
+        while (stack.length > 0 && stack[stack.length - 1] < nums2[i]) {
+            map.set(stack.pop(),nums2[i])
         }
+        stack.push(nums2[i])
     }
-    return nums1
+
+    for (let i = 0; i < len1; i++) {
+        nums1[i] = map.get(nums1[i]) || -1;
+    }
+    return nums1;
 };
 // @lc code=end
 
