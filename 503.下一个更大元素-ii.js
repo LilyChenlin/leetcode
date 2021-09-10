18370d21cdf5dbb906ac86dc3c298ee3d4660609
LilyChenlin <906791425@qq.com>
2021-09-11 00:04:26 +0800

    feat: 503下一个更大元素

---
+++ 503.下一个更大元素-ii.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=503 lang=javascript
+ *
+ * [503] 下一个更大元素 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElements = function(nums) {
+    let len = nums.length;
+    let res = new Array(len).fill(-1);
+    let stack = [];
+    for (let i = 0; i < len * 2 - 1; i++) {
+        while (stack.length > 0 && nums[stack[stack.length - 1]] < nums[i % len]) {
+            res[stack.pop()] = nums[i % len]
+        }
+        stack.push(i % len)
+    }
+    return res;
+};
+// @lc code=end
+
