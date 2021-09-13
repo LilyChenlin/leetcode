7ac86d79f4c5de64c77a3cffebe2d92776fe504f
LilyChenlin <906791425@qq.com>
2021-09-13 23:07:37 +0800

    feat: 316去除重复字母

--- 316.去除重复字母.js
+++ 316.去除重复字母.js
@@ -10,32 +10,24 @@
  * @return {string}
  */
 var removeDuplicateLetters = function(s) {
-    let res = '';
-    let dupRes = new Array(); // 存放去重的结果
-    const inStack = new Array(26).fill(false);
+    let newSet = new Set();
+    let stack = [];
+    let newMap = new Map();
+    for (let i = 0; i < s.length; i++) {
+        newMap.set(s[i], newMap.get(s[i]) + 1 || 1);
+    }
 
-    // 维护一个map记录s字符串中每个字符的数量
-    let sMap = new Map();
     for (let i = 0; i < s.length; i++) {
-        sMap.set(s[i], sMap.has(s[i]) ? sMap.get(s[i]) + 1 : 1);
-    }
-    for (let i = 0; i < s.length; i++) {
-        let item = s[i];
-        sMap.set(item, sMap.has(item) ? sMap.get(item) - 1 : -1);
-        // 如果字符存在inStack中
-        if (inStack[item]) continue;
-        while(dupRes.length > 0 && dupRes[dupRes.length - 1] > item) {
-            if (sMap.get(dupRes[dupRes.length - 1]) === 0) {
-                break;
+        if (!newSet.has(s[i])) {
+            while(stack.length && stack[stack.length - 1] > s[i] && newMap.get(stack[stack.length - 1] ) > 0) {
+                newSet.delete(stack.pop());
             }
-            inStack[dupRes[dupRes.length - 1]] = false;
-            dupRes.pop();
+            newSet.add(s[i]);
+            stack.push(s[i])
         }
-        dupRes.push(item)
-        inStack[item] = true;
+        newMap.set(s[i], newMap.get(s[i]) -1 || -1);
     }
-    res = dupRes.join('');
-    return res;
+    return stack.join('');
 };
 // @lc code=end
 
