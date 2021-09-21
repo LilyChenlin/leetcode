43b5ebbe7bd4bcd656ff412eec1c86a02c9fdf6f
LilyChenlin <906791425@qq.com>
2021-09-21 22:30:16 +0800

    重温三数之和

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -11,29 +11,26 @@
  */
 var threeSum = function(nums) {
     let res = [];
+    if (nums.length < 3 || !nums) return res;
     let len = nums.length;
-    if (len < 3 || !nums) return res;
-    //升序
-    nums.sort((a, b) => a - b);
-
-    for(let i = 0; i < len; i++) {
-        if (nums[i] > 0) break;
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < len; i++) {
+        if (nums[i] > 0) return res;
+        if (nums[i] == nums[i - 1] && i > 0) continue;
         let left = i + 1, right = len - 1;
-        while(left < right) {
+        while (left < right) {
             let sum = nums[i] + nums[left] + nums[right];
             if (sum == 0) {
-                res.push([nums[i],nums[left],nums[right]]);
-                while(left < right && nums[left] == nums[left + 1])  left++;
-                while(left < right && nums[right] == nums[right - 1])  right--;
+                res.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right + 1]) right--;
                 left++;
-                right--;
-            } else if (sum > 0) {
                 right--;
             } else if (sum < 0) {
                 left++;
+            } else {
+                right--;
             }
-
         }
     }
     return res;
