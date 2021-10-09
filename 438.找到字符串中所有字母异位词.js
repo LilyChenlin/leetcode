0269f668b6f7e16e3cb63fb6afd3407ab019700d
LilyChenlin <906791425@qq.com>
2021-10-09 23:10:57 +0800

    feat: 滑动窗口 双指针实现找到字符串中所有字母异位词

--- 438.找到字符串中所有字母异位词.js
+++ 438.找到字符串中所有字母异位词.js
@@ -11,25 +11,24 @@
  * @return {number[]}
  */
 var findAnagrams = function(s, p) {
-    let need = new Map (); 
-    for (let i = 0; i < p.length; i++) {
-        need.set(p[i], need.has(p[i]) ? need.get(p[i]) + 1 : 1);
-    }
-    let window = new Map();
     let left = 0, right = 0;
-    let valid = 0;
+    let need = new Map();
+    for (let i = 0; i < p.length; i++) {
+        need.set(p[i], need.get(p[i]) + 1 || 1)
+    }
     let result = [];
+    let valid = 0; // 验证字母是否都满足
+    let window = new Map();
     while (right < s.length) {
-        let a = s[right];
-        right++;
-        if (need.has(a)) {
-            window.set(a, window.has(a) ? window.get(a) + 1 : 1);
-            if (window.get(a) === need.get(a)) {
+        let temp = s[right];
+        if (need.has(temp)) {
+            window.set(temp, window.get(temp) + 1 || 1);
+            if (window.get(temp) === need.get(temp)) {
                 valid++;
             }
         }
-
-        while (right - left >= p.length) {
+        right++;
+        if (right - left >= p.length) {
             if (valid === need.size) {
                 result.push(left);
             }
@@ -37,15 +36,13 @@ var findAnagrams = function(s, p) {
             left++;
             if (need.has(d)) {
                 if (window.get(d) === need.get(d)) {
-                    valid--
+                    valid--;
                 }
-                window.set(d, window.has(d) ? window.get(d) - 1 : -1);
-
+                window.set(d, window.get(d) - 1 || -1)
             }
         }
-
     }
-    return result
+    return result;
 };
 // @lc code=end
 
