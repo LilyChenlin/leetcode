561b68cf76a198e9577ef5befce820dc3507ec60
LilyChenlin <906791425@qq.com>
2021-10-14 22:38:57 +0800

    feat: 86分隔链表

---
+++ 86.分隔链表.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=javascript
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    let small = new ListNode(0);
+    const smallNode = small;
+    let large = new ListNode(0);
+    const largeNode = large;
+
+    while (head !== null) {
+        if (head.val < x) {
+            small.next = head;
+            small = small.next;
+        } else {
+            large.next = head;
+            large = large.next;
+        }
+        head = head.next;
+    }
+    large.next = null;
+    small.next = largeNode.next;
+    return smallNode.next;
+};
+// @lc code=end
+
