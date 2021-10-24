4d9c92a15c64fd0ce1d7ad840493bc2604e36804
LilyChenlin <906791425@qq.com>
2021-10-24 21:36:42 +0800

    457.环形数组是否存在循环 - 优化

--- 457.环形数组是否存在循环.js
+++ 457.环形数组是否存在循环.js
@@ -31,5 +31,32 @@ var checked = (start, n, nums) => {
         k++;
     }
 }
+
+/**
+ * 解法二
+ */
+var circularArrayLoop = function (nums) {
+    let n = nums.length;
+    let vis = new Array(nums.length).fill(0);
+    for (let start = 0, idx = 1; start < n; start++, idx++) {
+        // 如果已这个为起点的被标记过
+        if (vis[start] !== 0) continue;
+        let cur = start;
+        let flag = nums[cur] > 0;
+        while (true) {
+            let next = ((cur + nums[cur]) % n + n) % n;
+            if (cur == next) break;
+            if (vis[next] !== 0) {// 说明被标记过
+                if (vis[next] !== idx) break;
+                else return true;
+            }
+            if (flag && nums[next] < 0) break;
+            if (!flag && nums[next] > 0) break;
+            vis[next] = idx;
+            cur = next;
+        }
+    }
+    return false
+}
 // @lc code=end
 
