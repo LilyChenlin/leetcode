aa3898df02abfcd61ff0545b53bb4c0922f29a07
LilyChenlin <906791425@qq.com>
2021-10-25 19:13:12 +0800

    feat: 类比链表实现287.寻找重复数

--- 287.寻找重复数.js
+++ 287.寻找重复数.js
@@ -9,15 +9,22 @@
  * @param {number[]} nums
  * @return {number}
  */
+// 类比链表 快慢指针
 var findDuplicate = function (nums) {
-    let n = nums.length;
-    for (let i = 0; i < n; i++) {
-        let start = i + 1, cur = nums[i];
-        while (start < n) {
-            if (nums[start] == cur) return cur;
-            else start++;
-        }
+    let slow = 0, fast = 0;
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
     }
+    let pre1 = 0, pre2 = slow;
+    while (pre1 !== pre2) {
+        pre1 = nums[pre1];
+        pre2 = nums[pre2];
+    }
+    return pre1;
 };
 // @lc code=end
 
