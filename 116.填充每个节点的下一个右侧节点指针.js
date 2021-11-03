0b166d44d552e2737bd08ddf347faba4f8fd4ac8
LilyChenlin <906791425@qq.com>
2021-11-03 22:53:29 +0800

    feat: 116.填充每个节点的下一个右侧节点指针 - 巩固

--- 116.填充每个节点的下一个右侧节点指针.js
+++ 116.填充每个节点的下一个右侧节点指针.js
@@ -72,23 +72,31 @@
  */
 
 /**
+ * @description 递归
  * @param {Node} root
  * @return {Node}
  */
 var connect = function(root) {
     if (root == null) return null;
-    connectTwoNode (root.left, root.right);
+    connectTwoNode(root.left, root.right);
     return root;
 };
-var connectTwoNode = function (left, right) {
-    if (left == null || right == null) return;
 
-    // 将传入的两个节点连接
+/**
+ * @description 理解为连接两个node
+ */
+
+let connectTwoNode = (left, right) => {
+    if (left == null || right == null) return ;
+
+    // 前序遍历 
     left.next = right;
-    connectTwoNode(left.left, left.right)
-    connectTwoNode(right.left, right.right)
 
-    connectTwoNode(left.right, right.left)
+    connectTwoNode(left.left, left.right);
+    connectTwoNode(right.left, right.right);
+
+    connectTwoNode(left.right, right.left);
+
 }
 // @lc code=end
 
