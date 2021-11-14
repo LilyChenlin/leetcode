f8a8046958bb236c63d36132fa35f8ebaae2b430
LilyChenlin <906791425@qq.com>
2021-11-14 22:20:35 +0800

    feat: 144.二叉树的前序遍历 - 递归

---
+++ 144.二叉树的前序遍历.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let res = [];
+
+    let loop = (root) => {
+        if (root == null) return;
+        res.push(root.val);
+        loop(root.left)
+        loop(root.right)
+    }
+    loop(root);
+    return res;
+};
+// @lc code=end
+
