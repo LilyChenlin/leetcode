c423061e48106b66d3e460367e275112c40d2771
LilyChenlin <906791425@qq.com>
2021-11-17 23:23:26 +0800

    fix: #297 二叉树的序列化与反序列化

--- 297.二叉树的序列化与反序列化.js
+++ 297.二叉树的序列化与反序列化.js
@@ -24,25 +24,14 @@ function TreeNode(val) {
  * @param {TreeNode} root
  * @return {string}
  */
-var serialize = function(root) {
-    let result = [];
-    helper(root, result);
-    return JSON.stringify(result);
-};
-
-var helper = (root, result) => {
-    // 前序遍历序列化
-    // if (root == null) return result.push('#');
-    // result.push(root.val);
-    // helper(root.left, result);
-    // helper(root.right, result);
-
-    //后序遍历
-    if (root == null) return result.push('#');
-    helper(root.left, result);
-    helper(root.right, result);
-    result.push(root.val);
-}   
+const serialize = (root) => {
+    if (root == null) {                  
+        return 'X';
+    }
+    const left = serialize(root.left);
+    const right = serialize(root.right);
+    return root.val + ',' + left + ',' + right;
+};  
 
 /**
  * Decodes your encoded data to tree.
@@ -50,37 +39,22 @@ var helper = (root, result) => {
  * @param {string} data
  * @return {TreeNode}
  */
-var deserialize = function(data) {
+const deserialize = (data) => {
+    const list = data.split(',');
 
-    data = JSON.parse(data);
-    return helperD(data)
-    
-    
+    const buildTree = (list) => {
+        const rootVal = list.shift();
+        if (rootVal == "X") {
+            return null;
+        }
+        const root = new TreeNode(rootVal); 
+        root.left = buildTree(list);        
+        root.right = buildTree(list);
+        return root;
+    };
 
+    return buildTree(list);
 };
-var helperD = (data) => {
 
-    // 前序遍历反序列化
-    // const val = data.shift(); 
-    // if (val === '#') return null;
-    // const node = new TreeNode(val);
-    // node.left = helperD(data);
-    // node.right = helperD(data)
-    // return node;
-
-    // 后序遍历 后序遍历是左右中的顺序，所以先找到根节点应在数组中pop最后一个数字 
-    const val  = data.pop();
-    if (val === '#') return null;
-    const node = new TreeNode(val);
-    node.right = helperD(data);
-    node.left = helperD(data);
-    return node
-
-
-}   
-/**
- * Your functions will be called as such:
- * deserialize(serialize(root));
- */
 // @lc code=end
 
