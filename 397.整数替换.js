85199659803f60ae92bde2d16ef538485cfc14d7
LilyChenlin <906791425@qq.com>
2021-12-02 23:04:17 +0800

    :sparkles: feat: 397.整数替换

---
+++ 397.整数替换.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=397 lang=javascript
+ *
+ * [397] 整数替换
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerReplacement = function (n) {
+    if (n === 1) return 0;
+    if (n % 2 === 0) {
+        return 1 + integerReplacement(n / 2)
+    } 
+    return 2 + Math.min(integerReplacement(Math.floor(n / 2)), integerReplacement(Math.floor(n / 2) + 1))
+};
+// @lc code=end
+
