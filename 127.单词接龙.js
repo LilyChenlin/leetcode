1870bb9e46b6531ffed4233644d5597192fdb306
LilyChenlin <906791425@qq.com>
2021-12-27 23:17:30 +0800

    :sparkles:feat: 127.单词接龙 双向广度遍历

--- 127.单词接龙.js
+++ 127.单词接龙.js
@@ -11,32 +11,57 @@
  * @param {string[]} wordList
  * @return {number}
  */
-var ladderLength = function(beginWord, endWord, wordList) {
+var ladderLength = function (beginWord, endWord, wordList) {
+    // The First Step
     const wordSet = new Set(wordList);
-    let step = 1;
-    let queue = [beginWord];
+    if (wordSet.size === 0 || !wordSet.has(endWord)) return 0;
+    // Second
+    let visited = new Set(),
+        beginVisited = new Set(),
+        endVisited = new Set(),
+        nextLevelVisited = new Set()
+    step = 1;
+    beginVisited.add(beginWord);
+    endVisited.add(endWord)
+    const changeWordEveryOneLetter = (word) => {
+        let wordArr = word.split('');
+        for (let i = 0; i < wordArr.length; i++) {
+            const originChar = wordArr[i];
 
-    while (queue.length) {
-        const next = [];
-
-        for (let item of queue) {
-            if (item === endWord) return step;
-            for (let i = 0; i < item.length; i++) {
-                
-                for (let j = 0; j < 26; j++) {
-                    const newWord = item.slice(0, i) + String.fromCharCode(j + 97) + item.slice(i + 1);
-
-                    if (wordSet.has(newWord)) {
-                        next.push(newWord);
-                        wordSet.delete(newWord)
+            for (let j = 0; j < 26; j++) {
+                let c = String.fromCharCode(j + 97);
+                if (originChar == c) continue;
+                wordArr[i] = c;
+                const nextWord = wordArr.join('');
+                if (wordSet.has(nextWord)) {
+                    if (endVisited.has(nextWord)) { return true };
+                    if (!visited.has(nextWord)) {
+                        nextLevelVisited.add(nextWord);
+                        visited.add(nextWord)
                     }
                 }
             }
+            wordArr[i] = originChar;
         }
+        return false;
+    }
+
+    while (beginVisited.size > 0 && endVisited.size > 0) {
+        if (beginVisited.size > endVisited.size) {
+            const temp = beginVisited;
+            beginVisited = endVisited
+            endVisited = temp;
+        }
+
+        nextLevelVisited = new Set();
+        for (let word of beginVisited) {
+            if (changeWordEveryOneLetter(word)) return step + 1;
+        }
+        beginVisited = nextLevelVisited;
         step++;
-        queue = next;
     }
     return 0;
 };
+
 // @lc code=end
 
