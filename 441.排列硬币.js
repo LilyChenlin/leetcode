a1748945e392ee99c456d3a2a915c64f23e4b577
LilyChenlin <906791425@qq.com>
2022-01-11 23:37:31 +0800

    :sparkles:feat: 441.排列硬币(二分查找)

---
+++ 441.排列硬币.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=441 lang=javascript
+ *
+ * [441] 排列硬币
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+    let left = 1, right = n;
+    while (left < right) {
+        const mid = left + Math.round((right - left) / 2);
+        if (mid * (mid + 1) <= 2 * n) {
+            left = mid;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+};
+// @lc code=end
+
