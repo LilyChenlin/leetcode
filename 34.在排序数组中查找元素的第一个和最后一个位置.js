dfbb2f6df3c861cf8fa8de9448cc6443cf7299e7
LilyChenlin <906791425@qq.com>
2022-01-13 22:57:38 +0800

    :sparkles:feat: 34.在排序数组中查找元素的第一个和最后一个位置

--- 34.在排序数组中查找元素的第一个和最后一个位置.js
+++ 34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -12,31 +12,40 @@
  */
 
 // 即找出左边界和右边界值
-var searchRange = function(nums, target) {
+var searchRange = function (nums, target) {
     let res = [-1, -1];
-    const leftIndex = binarySearch(nums, target, true);
-    const rightIndex = binarySearch(nums, target, false) - 1;// 寻找右边界是寻找第一个大于target的数， 因此右边界需要将这个index-1
-    if (leftIndex <= rightIndex && rightIndex < nums.length && nums[leftIndex] === target && nums[rightIndex] === target) {
-        res = [leftIndex, rightIndex];
+    let left = 0, right = nums.length - 1;
+    let firstIdx = findFirstVal(left, right, target, nums);
+    if (nums[firstIdx] === target) {
+        let leftIdx = firstIdx, rightIdx = firstIdx;
+        while (nums[leftIdx - 1] === target) {
+            leftIdx--;
+        }
+        while (nums[rightIdx + 1] === target) {
+            rightIdx++;
+        }
+        res = [leftIdx, rightIdx]
+        return res;
+    } else {
+        return res;
     }
-    return res;
-}; 
 
-// 通过lower判断是左边届还是右边界 lower -> 左边界  大与等于即可
-var binarySearch = function(nums, target, lower) {
-    // right -> 左边界
-    // ans -> 有边界
-    let left = 0, right = nums.length - 1, ans = nums.length;
-    while (left <= right) {
-        const mid = Math.floor((right + left) / 2);
-        if (nums[mid] > target || (nums[mid] >= target && lower)) {
-            right = mid - 1;
-            ans = mid; 
+
+
+};
+
+let findFirstVal = (left, right, target, nums) => {
+    while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (nums[mid] >= target) {
+            right = mid;
         } else {
             left = mid + 1;
         }
     }
-    return ans;
+    return left;
 }
+
+
 // @lc code=end
 
