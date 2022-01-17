e28b3dc67c21cb71c0d947da96d580d4170ee805
LilyChenlin <906791425@qq.com>
2022-01-17 22:07:09 +0800

    :sparkles:feat: 300.最长递增子序列(动态规划)

---
+++ 300.最长递增子序列.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=typescript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+function lengthOfLIS(nums: number[]): number {
+    const n = nums.length;
+    const dp = new Array(n).fill(1);
+    let res = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+        res = Math.max(res, dp[i])
+    }
+    return res;
+};
+// @lc code=end
+
