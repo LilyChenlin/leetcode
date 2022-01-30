a9b70a879b4522851bdbd4d71a569c921b996431
LilyChenlin <906791425@qq.com>
2022-01-30 19:54:25 +0800

    :sparkles:feat: 70.爬楼梯

---
+++ 70.爬楼梯.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=typescript
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+    let dp = new Array(n + 1);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n]
+};
+// @lc code=end
+
