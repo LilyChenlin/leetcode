e52f98add138b1d3d4a6855bf0ad363a5e35f534
LilyChenlin <906791425@qq.com>
2022-02-09 22:48:12 +0800

    :sparkles:feat: 746.使用最小花费爬楼梯

---
+++ 746.使用最小花费爬楼梯.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=typescript
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+// @lc code=start
+function minCostClimbingStairs(cost: number[]): number {
+    let n = cost.length;
+    let dp = new Array(n);
+    dp[0] = dp[1] = 0;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+    return dp[n]
+};
+// @lc code=end
+
