20d0c3923e56d6f89ef83b7f7a87304127474a2f
LilyChenlin <906791425@qq.com>
2022-02-10 16:51:39 +0800

    :sparkles:feat: 344.反转字符串

---
+++ 344.反转字符串.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=typescript
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    let left = 0, right = s.length - 1;
+    while (left < right) {
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+};
+// @lc code=end
+
