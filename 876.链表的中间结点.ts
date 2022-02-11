f6098454e160597d487abae6bd10a5db6b11902f
LilyChenlin <906791425@qq.com>
2022-02-11 14:00:25 +0800

    :sparkles:feat: 876.链表的中间结点

---
+++ 876.链表的中间结点.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=typescript
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+    let slow = head, fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+};
+// @lc code=end
+
