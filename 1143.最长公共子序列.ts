4de7ad6985634248ce08ab30b97a58c5d9b1ecf1
LilyChenlin <906791425@qq.com>
2022-02-26 23:15:06 +0800

    :sparkles:feat: 1143.最长公共子序列

---
+++ 1143.最长公共子序列.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=typescript
+ *
+ * [1143] 最长公共子序列
+ */
+
+// @lc code=start
+function longestCommonSubsequence(text1: string, text2: string): number {
+    let m = text1.length, n = text2.length;
+    let dp = new Array(m + 1).fill(0).map(item => new Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n]
+};
+// @lc code=end
+
