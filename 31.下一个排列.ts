31c38dd413b2239da212c2827dc968e4ea331220
LilyChenlin <906791425@qq.com>
2022-03-12 23:06:20 +0800

    :sparkles:feat: 31.下一个排列

---
+++ 31.下一个排列.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=typescript
+ *
+ * [31] 下一个排列
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function nextPermutation(nums: number[]): void {
+    // 从后往前遍历
+    let i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+    if (i >= 0) {
+        let j = nums.length - 1;
+        while (j >= 0 && nums[j] <= nums[i]) {
+            j--;
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]]
+    }
+    let start = i + 1, end = nums.length - 1;
+    while (start < end) {
+        [nums[start], nums[end]] = [nums[end], nums[start]];
+        start++;
+        end--;
+    }
+};
+// @lc code=end
+
