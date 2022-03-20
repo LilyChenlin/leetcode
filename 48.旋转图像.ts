cc4e2d3c0b860639991b10897b4684448f6ee912
LilyChenlin <906791425@qq.com>
2022-03-20 22:10:53 +0800

    :sparkles:feat: 48.旋转图像

---
+++ 48.旋转图像.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=typescript
+ *
+ * [48] 旋转图像
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+    let n = matrix.length;
+    // 水平对折
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        for (let j = 0; j < n; j++) {
+            [matrix[i][j], matrix[n - i - 1][j]] = [matrix[n - i - 1][j], matrix[i][j]]
+        }
+    }
+
+    // 对角线翻折
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i],matrix[i][j]];
+        }
+    }
+};
+// @lc code=end
+
