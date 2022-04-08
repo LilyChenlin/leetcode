e4eed195976782d644f3ec2f509309ff3c4458e3
LilyChenlin <906791425@qq.com>
2022-04-08 21:53:20 +0800

    :sparkles:feat: 104.二叉树的最大深度

---
+++ 104.二叉树的最大深度.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=typescript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    if (root == null) return 0;
+    if (!root.left && !root.right) return 1;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+// @lc code=end
+
