872570e006e4194608da8b37f3bbcb0c914d7b3a
LilyChenlin <906791425@qq.com>
2022-04-17 22:04:29 +0800

    :sparkles:feat: 141. 环形链表

---
+++ 141.环形链表.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=typescript
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    let slow = head, fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (fast == slow) return true;
+    }
+    return false;
+};
+// @lc code=end
+
