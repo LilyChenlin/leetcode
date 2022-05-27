b76e28ab135ab55a59240eb6e1607f2002543ded
LilyChenlin <906791425@qq.com>
2022-05-27 22:58:04 +0800

    :sparkles:feat: 538.把二叉搜索树转换为累加树

---
+++ 538.把二叉搜索树转换为累加树.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=typescript
+ *
+ * [538] 把二叉搜索树转换为累加树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function convertBST(root: TreeNode | null): TreeNode | null {
+    let sum = 0;
+
+    let loop = (root) => {
+        if (root == null) return 0;
+        loop(root.right);
+        sum += root.val;
+        root.val = sum;
+        loop(root.left);
+    }
+
+    loop(root);
+    return root;
+};
+// @lc code=end
+
